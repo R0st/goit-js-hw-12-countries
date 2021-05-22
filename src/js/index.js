7350aa64b2194dfa5df6a236a005b51e297ab58a
R0st <dotarost@mail.ru>
2021-05-22 23:45:51 +0300

    last 1 country

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 
 var debounce = require('lodash.debounce');
-const { error } = require('@pnotify/core');
-
-import '@pnotify/core/dist/Material.css';
+// const { error } = require('@pnotify/core');
+import { error } from '@pnotify/core';
+// import '@pnotify/core/dist/Material.css';
+import '@pnotify/core/dist/BrightTheme.css';
 // import { render } from 'sass';
 // import './sass/main.scss';
-import '../sass/main.scss';
+import '../style.css';
 import countryCardTpl from '../templates/country-card.hbs';
 import countriesList from '../templates/countriesList.hbs';
 import API from './fetchCountries';
 import getRefs from './get-refs';
-
 const refs = getRefs(); 
     
 refs.searchForm.addEventListener('input', debounce(onSearch, 500));
@@ -18,6 +18,7 @@ refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(event) {
     event.preventDefault();
+    clearContainer();
     const searchQuery = event.target.value;
 
     API.fetchCountries(searchQuery)
@@ -26,19 +27,18 @@ function onSearch(event) {
                 error({
                     text: 'Too many matches found. Please enter a more specific query!',
                 });
-            } else if (2 <= countries.length <= 10) {
-                renderCountriesList;
-            } else if (countries.length = 1) {
-                renderCountryCard;
+            } else if (1< countries.length <= 10) {
+                 renderCountriesList(countries);
+            } else if (countries.length === 1) {
+                 renderCountryCard(countries);
             }
         })
         .catch(onFetchError);
 }
 
-function renderCountryCard(country) {
-    // console.log('this is thennnnnnnnnnnnnnnnn');
-        const markup = countryCardTpl(country);
-        refs.countryContainer.innerHTML = markup;
+function renderCountryCard(countries) {
+    const markup = countryCardTpl(countries);
+    refs.countryContainer.innerHTML = markup;
 }
     
 function renderCountriesList(countries) {
@@ -46,7 +46,11 @@ function renderCountriesList(countries) {
     refs.countryContainer.innerHTML = markupList;
 }
 
-    function onFetchError(error) {
-        console.log(error);
-            alert('Введите коректное название и будет вам страна :)');
-    }
+function onFetchError(error) {
+    console.log(error);
+    // alert('Введите коректное название и будет вам страна :)');
+}
+    
+function clearContainer() {
+    refs.countryContainer.innerHTML = '';
+}
