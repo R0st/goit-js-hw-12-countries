98668189dce70c320975897c1d84f81c60152ead
R0st <dotarost@mail.ru>
2021-05-23 10:55:42 +0300

    add card one country and pnotify with parametres

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 
 var debounce = require('lodash.debounce');
-// const { error } = require('@pnotify/core');
-import { error } from '@pnotify/core';
-// import '@pnotify/core/dist/Material.css';
+const { error } = require('@pnotify/core');
+
 import '@pnotify/core/dist/BrightTheme.css';
-// import { render } from 'sass';
-// import './sass/main.scss';
 import '../style.css';
 import countryCardTpl from '../templates/country-card.hbs';
 import countriesList from '../templates/countriesList.hbs';
@@ -17,7 +14,7 @@ refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 // refs.searchForm.addEventListener('input', onSearch);
 
 function onSearch(event) {
-    event.preventDefault();
+    // event.preventDefault();
     clearContainer();
     const searchQuery = event.target.value;
 
@@ -26,11 +23,18 @@ function onSearch(event) {
             if (countries.length > 10) {
                 error({
                     text: 'Too many matches found. Please enter a more specific query!',
+                    maxTextHeight: '100px',
+                    delay: 2000,
+                    closer: true,
+                    sticker: false,
+                    icon: false,
                 });
-            } else if (1< countries.length <= 10) {
-                 renderCountriesList(countries);
-            } else if (countries.length === 1) {
-                 renderCountryCard(countries);
+                return;
+            } if (countries.length>=2 && countries.length <= 10) {
+                renderCountriesList(countries);
+                return;
+            } if (countries.length === 1) {
+                 return renderCountryCard(countries);
             }
         })
         .catch(onFetchError);
@@ -46,9 +50,14 @@ function renderCountriesList(countries) {
     refs.countryContainer.innerHTML = markupList;
 }
 
-function onFetchError(error) {
-    console.log(error);
-    // alert('Введите коректное название и будет вам страна :)');
+function onFetchError() {
+    error ({
+        text: 'Error by fetch',
+        delay: 2000,
+        closer: true,
+        sticker: false,
+        icon: false,
+    });
 }
     
 function clearContainer() {
